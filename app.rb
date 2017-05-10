5a65e5ac595a9c5f076ede5d35d84cf712814dcd
Soon Van <cog@randomecho.com>
2017-05-09 23:00:29 -0400

    Move categories out to its own hash
    Should first make for a cleaner Slim template. Also should make
    it easier to reuse in a sense in other context of the display
    when later extracting out the records against what category
    they fall into.

--- app.rb
+++ app.rb
@@ -7,8 +7,28 @@ DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/slips.db")
 require_relative 'models/chit'
 DataMapper.finalize
 
+categories = {
+  eat_out: 'Eating out',
+  groceries: 'Groceries',
+  car: 'Car',
+  gas: 'Gas',
+  pets: 'Pets',
+  entertainment: 'Entertainment',
+  health: 'Health',
+  home: 'Home',
+  clothes: 'Clothes',
+  comms: 'Comms',
+  toys: 'Toys',
+  books: 'Books',
+  travel: 'Travel',
+  rent: 'Rent',
+  gifts: 'Gifts',
+  donations: 'Donations'
+}
+
 get '/' do
   @chits = Chit.all(:order => [:purchase_date.desc, :created_at.desc])
+  @categories = categories
   slim :index
 end
 
